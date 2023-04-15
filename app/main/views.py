451eb4612c67f18bf6b299eea15da96183972880
zenor0 <zenor0@outlook.com>
2023-04-15 21:44:10 +0800

    api defination

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,26 @@
 from flask import redirect, request, url_for
-
+from .models import BaseResponse
+from app.models import Message
 from app.main import main
+from app import db
 
-@main.route("/")
+@main.route("/", methods=['GET', 'POST'])
 def index():
-    return 'hello!'
+    result = Message.query.filter_by(detail='hello').all()
+    mes = Message(room_id=1, sender_id=1, detail=request.json.get('username')).save()
+    # return {"all": [i.to_dict() for i in result]}
+    return BaseResponse(code=222, message='hi', data={"msg": [i.to_dict() for i in result]}).dict()
+
+
+@main.route('/<int:goods_id>', methods=['POST'])
+def establish_room(goods_id):
+    return BaseResponse().dict()
+
+@main.route('/log/<int:room_id>', methods=['GET'])
+def pull_message_logs(room_id):
+    return BaseResponse().dict()
+
+@main.route('/', methods=['GET'])
+def get_chat_list(room_id):
+    return BaseResponse().dict()
+
